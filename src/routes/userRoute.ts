bbc9c6f328ca92189321424440b19ad92c6dcd6b
Nabil-bk-dev <nabilbakhelal64@gmail.com>
2025-06-10 17:24:30 +0200

    initial commit + User Feature

---
+++ src/routes/userRoute.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { login, register } from '../services/userService';
+
+const router = express.Router();
+
+router.post('/register', async (req,res) => {
+    const {firstName,lastName,email,password} = req.body;
+    const result = await register({firstName,lastName,email,password});
+    res.status(result.statuscode).send(result.data);
+})
+
+router.post('/login', async (req,res) => {
+    const {email,password} = req.body;
+    const result = await login({email,password});
+    res.status(result.statuscode).send(result.data);
+})
+
+export default router;
---
+++ src/services/userService.ts
@@ -0,0 +1,46 @@
+import { userModel } from "../models/userModel";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+interface RegisterParams {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export const register = async ({firstName,lastName,email,password} : RegisterParams ) => {
+    const findUser = await userModel.findOne({email});
+    if(findUser){
+        return{ data : " User already exist " , statuscode : 400 };
+    }else{
+        const hashedPassword =  await bcrypt.hash(password,10);
+        const newUser = new userModel({firstName,lastName,email,password:hashedPassword});
+        await newUser.save();
+        return { data : generateJWT({firstName,lastName,email}) , statuscode : 200 };
+    }
+}
+
+interface LoginParams {
+    email: string;
+    password: string;
+}
+
+export const login = async ({email,password} : LoginParams) => {
+    const findUser = await userModel.findOne({email});
+    if(!findUser){
+        return{ data : "incorect email or password" , statuscode : 400 };
+    }
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
+    if(passwordMatch){
+        return {data : generateJWT({firstName : findUser.firstName,lastName : findUser.lastName,email}) , statuscode : 200 };
+    }
+    return { data : "incorect email or password" , statuscode : 400 };
+}
+
+const generateJWT = (data : any) => {
+    return jwt.sign(data , 'KT8EMy0O8vZ6wPJNVcJO8aqds3c5A3zY');
+}
+
+
+
